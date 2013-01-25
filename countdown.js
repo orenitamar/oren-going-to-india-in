3381cf5972ed3ec8fc370a8e2db82f537237d9d4
Oren Itamar <oren@doit9.com>
2013-01-25 14:22:52 +0200

    finally working

--- countdown.js
+++ countdown.js
@@ -1,10 +1,6 @@
 var now = new Date();
-var now_utc = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(),  now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
-alert(now_utc)
-var departure = Date.UTC(2013,2,21,7,50,0);
-var landing = Date.UTC(2013,3,21,23,25,00) ;
-var back = Date.UTC(2013,4,12,12,45,0);
-var time_to_departure = (departure - now_utc)/1000;
+var departure = new Date("2013-03-21T07:50:00Z");
+var time_to_departure = (departure - now)/1000 ;
 
 function countdown(){
 	if (time_to_departure>0){	
@@ -14,5 +10,7 @@ function countdown(){
 		minutes = Math.floor((time_to_departure % 3600)/60)
 		seconds = Math.floor(time_to_departure % 60);
 		document.getElementById("counter").innerHTML = days+" Days "+hours+" Hours "+minutes+" Minutes "+seconds+" Seconds";
+	}else{
+		document.getElementById("text").innerHTML = "Oren is already in India, probably eating chicken biryani...";
 	}
 }
