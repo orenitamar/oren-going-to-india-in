c03a02dd48552c04302ea65d4c5538a1e8dfea45
Rotem Mizrachi <rotemmiz@gmail.com>
2013-01-28 11:40:22 +0200

    Update countdown.js
    What about when you're on the plane ?

--- countdown.js
+++ countdown.js
@@ -1,6 +1,8 @@
 var now = new Date();
 var departure = new Date("2013-03-21T07:50:00Z");
+var landing = new Date("2013-03-21T23:25:00Z");
 var time_to_departure = (departure - now)/1000 ;
+var time_to_land =  (landing - now)/1000 ;
 
 function handleNumber(number) {
 	if (number.toString().length === 1) {
@@ -10,7 +12,7 @@ function handleNumber(number) {
 }
 
 function countdown(){
-	if (time_to_departure>0){	
+	if (time_to_departure > 0){	
 		time_to_departure -= 1;
 		days = Math.floor(time_to_departure/86400);
 		hours = Math.floor((time_to_departure % 86400)/3600);
@@ -20,8 +22,12 @@ function countdown(){
 		document.getElementById("hoursNumber").innerHTML = handleNumber(hours);
 		document.getElementById("minutesNumber").innerHTML = handleNumber(minutes);
 		document.getElementById("secondsNumber").innerHTML = handleNumber(seconds);
-		
-	}else{
+
+	}else if (time_to_departure < 0 && time_to_land > 0) {
+		document.getElementById("text").innerHTML = "Oren is on a plain, probably seating in the best rated seat for it's price class," +
+		"since Oren is a smart lad, he knows how to check seats in SeatGuru";
+	}	
+	else{
 		document.getElementById("text").innerHTML = "Oren is already in India, probably eating chicken biryani...";
 	}
 }
